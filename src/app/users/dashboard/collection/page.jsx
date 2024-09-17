f449ef3f690f780f24fde3e7f4ae7830020c8def
EvanescenT07 <zulfikar.ahmad12@yahoo.com>
2024-09-17 21:10:15 +0700

    Change NPM into Bun to for performance improvement

--- src/app/users/dashboard/collection/page.jsx
+++ src/app/users/dashboard/collection/page.jsx
@@ -1,8 +1,7 @@
 const Page = () => {
-  return <div>Hello Collection</div>;
+  return (
+    <div>Hello Collection</div>
+  );
 };
 
 export default Page;
-
-// TO DO : Make Collection Page
-// TO DO : Make Comment Page
